8a5ac5f57d2919ab4532f67c1b085c72191748d5
RezzyPhill <phillipferguson2015@gmail.com>
2025-02-06 00:32:24 -0500

    Update data_cleaning.py
    finished data_cleaning for now

--- data_cleaning.py
+++ data_cleaning.py
@@ -9,11 +9,25 @@ print(workload_data.isnull().sum())
 print("\nMissing values in demographics_data:")
 print(demographics_data.isnull().sum())
 
-#Drop the rows with too many missing values
+#Dropping the rows with too many missing values
 workload_data = workload_data.dropna(subset=['PLAYER_ID', 'PTS'])
 demographics_data = demographics_data.dropna(subset=['PERSON_ID', 'POSITION'])
 
-#  Fill missing num values with col mean
+#  Filling missing num values with col mean
+numeric_cols_workload = workload_data.select_dtypes(include=['float64'])  # Select numerical columns
+workload_data[numeric_cols_workload.columns] = numeric_cols_workload.fillna(numeric_cols_workload.mean())
 
-numeric_cols = workload_data.select_dtypes(include=['float64', 'int64']).columns
-workload_data[numeric_cols]
+numeric_cols_demographics = demographics_data.select_dtypes(include=['float64'])
+demographics_data[numeric_cols_demographics.columns] = numeric_cols_demographics.fillna(numeric_cols_demographics.mean())
+
+# Changing the cols with mv to "Unknown"
+categorical_cols_workload = workload_data.select_dtypes(include=['object'])
+workload_data[categorical_cols_workload.columns] = categorical_cols_workload.fillna("Unknown")
+
+categorical_cols_demographics = demographics_data.select_dtypes(include=['object'])
+demographics_data[categorical_cols_demographics.columns] = categorical_cols_demographics.fillna("Unknown")
+
+#Saving the cleaned datasets
+workload_data.to_csv('data/workload_data_cleaned.csv', index=False)
+demographics_data.to_csv('data/demographics_data_cleaned.csv', index=False)
+print("Data cleaning complete & Cleaned files saved!")
